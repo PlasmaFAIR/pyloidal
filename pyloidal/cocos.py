7cd16d4a57855f4ad3a34dc14509cf34a186c1f8
LiamPattinson <LiamPattinson@users.noreply.github.com>
2023-07-04 18:08:12 +0100

    Fix bug in identify_cocos
    Co-authored-by: Peter Hill <zed.three@gmail.com>

--- pyloidal/cocos.py
+++ pyloidal/cocos.py
@@ -131,7 +131,7 @@ def identify_cocos(
                     x,
                     minor_radii,
                 )
-                for x in (False, False)
+                for x in (True, False)
             )
         )
 
